f242873acf482c943e98d32dbe2980c7b6aedffa
Dominik Schöni <dominik@dominikschoeni.ch>
2022-05-16 19:26:57 +0200

    feat: vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
+    'vue/setup-compiler-macros': true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier'],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser'
   },
+  plugins: [],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'warn',
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'vue/attributes-order': 'off',
+    'vue/component-tags-order': 'off',
+    'vue/order-in-components': 'off'
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Build](https://github.com/kontera-ch/verify/actions/workflows/main.yml/badge.svg)
+
 # Kontera Verify
 
 Stand-alone application to verify Kontera-generated timestamping proofs with original files and their inclusion on the Tezos Blockchain.
